c217f35357a50845e205d971b0637bf823707f42
Rafael Montenegro <rafaelmrc3@gmail.com>
2022-08-19 19:38:57 -0300

    Adicionar arquivos de projeto.

---
+++ STFROTA/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STFROTA.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cnh { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string LoginCadastro { get; set; }
+    }
+}
---
+++ STFROTA/Models/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STFROTA.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string LoginCadastro { get; set; }
+    }
+}
